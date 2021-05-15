7fe52b70e50021f2aefb1b4bfd828ebb59335bbd
Aureen <yukiansff@gmail.com>
2021-05-15 20:14:04 +0200

    Rotated hexagons.

--- src/components/post/Preview.jsx
+++ src/components/post/Preview.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 function Preview({ posts }) {
-  // Reduce amount per row to give responsiveness.
+  const [organized, setOrganized] = useState();
+  const [amount, setAmount] = useState(3);
+
+  // Reorganize posts into several arrays to create the honeycomb effect.
+  useEffect(() => {
+    let even = true;
+    const array = [];
+    const copy = [...posts];
+
+    while (copy.length !== 0) {
+      if (even) {
+        array.push(copy.splice(0, amount));
+        even = !even;
+      }
+      array.push(copy.splice(0, amount - 1));
+    }
+    setOrganized(copy);
+  }, [posts]);
+
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const reorganized = [
+    [1, 2, 3],
+    [4, 5],
+    [6, 7, 8],
+    [9, 10],
+  ];
+
   return (
-    <Grid amount={3}>
-      {/* {posts.map((post, index) => (
-        <Item key={post._id} index={index + 1} amount={3}>
-          <Content>{post.title}</Content>
-        </Item>
-      ))} */}
-      {new Array(9).fill(0).map((elem, index) => (
-        <Item key={index} index={index + 1} amount={3}>
-          <Content>{index + 1}</Content>
+    <Grid amount={amount}>
+      {arr.map((post) => (
+        <Item
+          row={reorganized.findIndex((row) => row.includes(post)) + 1}
+          column={
+            reorganized
+              .find((row) => row.includes(post))
+              .findIndex((col) => col === post) + 1
+          }
+        >
+          <Content>{post}</Content>
         </Item>
       ))}
     </Grid>
@@ -26,27 +54,17 @@ const Grid = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
-  grid-template-columns: repeat(${(props) => props.amount}, 1fr 2fr) 1fr;
-  grid-gap: 2rem 4rem; // Grid-gap row is the gutter, grip-gap-column is gutter * 2.
+  grid-template-columns: repeat(${(props) => props.amount * 2}, 1fr);
+  grid-gap: 4rem 2rem;
 `;
 
 const Item = styled.li`
   position: relative;
-  grid-column: ${(props) => {
-      if (props.index % props.amount === 0) return props.amount * 2 - 1;
-      return (props.index % props.amount) * 2 - 1;
-    }} / span 3;
-  grid-row: ${(props) => {
-      if (
-        (props.index % props.amount) % 2 !== 0 ||
-        props.index % props.amount === 0
-      ) {
-        return Math.ceil(props.index / props.amount) * 2 - 1;
-      }
-      return Math.ceil(props.index / props.amount) * 2;
-    }} / span 2;
+  grid-row: ${(props) => 2 * props.row - 1} / span 3;
+  grid-column: ${(props) =>
+      props.row % 2 === 0 ? 2 * props.column : 2 * props.column - 1} / span 2;
   height: 0;
-  padding-bottom: 90%; // Gives a 9/10 ratio.
+  padding-bottom: 100%;
 `;
 
 const Content = styled.div`
@@ -56,13 +74,14 @@ const Content = styled.div`
   height: 100%;
   width: 100%;
   clip-path: polygon(
-    75% 0,
-    100% 50%,
-    75% 100%,
-    25% 100%,
-    0 50%,
-    25% 0
+    50% 0%,
+    0% 25%,
+    0% 75%,
+    50% 100%,
+    100% 75%,
+    100% 25%
   ); // Hexagon shape
   background: blue;
   opacity: 0.5;
+  font-size: 5rem;
 `;
